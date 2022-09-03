73fffd858aab7e6f9df4d27acdbb495c96329b6f
pritthwiraz <pritthwiraz@gmail.com>
2022-09-03 23:11:44 +0600

    first commit

--- JS/news.js
+++ JS/news.js
@@ -10,12 +10,46 @@ const displayNews = newsCategory => {
     newsCategory.forEach(category => {
         const newsDiv = document.createElement('div');
         newsDiv.innerHTML = `
-        <div class="d-flex justify-content-center">
-        <a class="navbar-brand p-2" href="#">${category.category_name}</a>
+               <a class="navbar-brand p-2" href="#">${category.category_name}</a>
         `;
         newsContainer.appendChild(newsDiv);
 
     })
 }
+// const newsDetails = () => {
+//     console.log('news details loaded')
+// }
+/*=================*/
+
+// const loadNewsDetail = (category) => {
+//     const url = `https://openapi.programming-hero.com/api/news/category/${category}`
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(category => console.log(category))
+// }
+
+
+/*
+const displayCountryDetail = country => {
+    const countryDetail = document.getElementById('country-detail')
+    countryDetail.innerHTML = `
+      <div class="row g-2">
+                    <div class="col-md-4">
+                        <img src="..." class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
+    `;
+
+}
+
+*/
 loadNews()
 
