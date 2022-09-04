56119b08c899fd52dcdde65efae61a7c7f277250
pritthwiraz <pritthwiraz@gmail.com>
2022-09-04 11:33:30 +0600

    js and html file updated

--- JS/news.js
+++ JS/news.js
@@ -38,60 +38,28 @@ const displayDetailNews = newsDetail => {
     // newsDetail.forEach(newsDetail => {
     const detailNewsDiv = document.createElement('div');
     detailNewsDiv.innerHTML = `
-        <div class="row g-0">
+        <div class="pb-4">
+            <div class="row g-0">
             <div class="col-md-4">
                 <img src="${newsDetail.thumbnail_url}" class="img-fluid rounded-start" alt="...">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title">${newsDetail.title}</h5>
-                    <p class="card-text">${newsDetail.details}</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <h5 class="card-title"><a href="">${newsDetail.title}</a></h5>
+                    <p class="card-text">${newsDetail.details}</p>                   
+                    <div class="d-flex justify-content-between">
+                    <p class="card-text">${newsDetail.author.name}</p>                    
+                    <p>Total View: ${newsDetail.total_view}</p>
+                    <button>Click</button>
+                    </div>
                 </div>
             </div>
-        </div>
+            </div>
+        
     `;
     detailNews.appendChild(detailNewsDiv);
 
 }
 
-
-
-
-// const newsDetails = () => {
-//     console.log('news details loaded')
-// }
-/*=================*/
-
-// const loadNewsDetail = (category) => {
-//     const url = `https://openapi.programming-hero.com/api/news/category/${category}`
-//     fetch(url)
-//         .then(res => res.json())
-//         .then(category => console.log(category))
-// }
-
-
-/*
-const displayCountryDetail = country => {
-    const countryDetail = document.getElementById('country-detail')
-    countryDetail.innerHTML = `
-      <div class="row g-2">
-            <div class="col-md-4">
-                <img src="..." class="img-fluid rounded-start" alt="...">
-            </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-            </div>
-       </div>
-    `;
-
-}
-
-*/
 loadNews()
 
