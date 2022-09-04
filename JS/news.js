05f0c0f1f32e4d482fe925c862c4bdd636ca55e1
pritthwiraz <pritthwiraz@gmail.com>
2022-09-04 09:03:48 +0600

    JS file updated

--- JS/news.js
+++ JS/news.js
@@ -32,9 +32,31 @@ const loadDetailNews = (category_id) => {
         .then(data => displayDetailNews(data.data[0]))
 }
 
-const displayDetailNews = news => {
-    console.log(news)
+const displayDetailNews = newsDetail => {
+    console.log(newsDetail)
+    const detailNews = document.getElementById('detail-news');
+    // newsDetail.forEach(newsDetail => {
+    const detailNewsDiv = document.createElement('div');
+    detailNewsDiv.innerHTML = `
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="${newsDetail.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${newsDetail.title}</h5>
+                    <p class="card-text">${newsDetail.details}</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+    `;
+    detailNews.appendChild(detailNewsDiv);
+
 }
+
+
+
 
 // const newsDetails = () => {
 //     console.log('news details loaded')
@@ -54,18 +76,18 @@ const displayCountryDetail = country => {
     const countryDetail = document.getElementById('country-detail')
     countryDetail.innerHTML = `
       <div class="row g-2">
-                    <div class="col-md-4">
-                        <img src="..." class="img-fluid rounded-start" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+            <div class="col-md-4">
+                <img src="..." class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
                                 additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                 </div>
+            </div>
+       </div>
     `;
 
 }
